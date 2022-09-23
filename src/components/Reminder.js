c0a93841291cb931d28e81d0ce3e66fe7e596055
snehathapaliya <shreyavshh@gmail.com>
2022-09-22 21:57:45 -0500

    Initializing the app

---
+++ src/components/Reminder.js
@@ -0,0 +1,15 @@
+import React, {} from "react";
+
+function Reminder(props) {
+    return (
+      <>
+        {props.showCompletedReminders ? (
+          <s>{props.reminder.title}</s>
+        ) : (
+          <span>{props.reminder.title}</span>
+        )}
+      </>
+    );
+  }
+  
+  export default Reminder;
